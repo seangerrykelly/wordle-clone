bb96940e493e82f2dd0c20d61ca891a7ae3ef2dd
Sean Kelly <seangerrykelly@gmail.com>
2025-10-16 15:19:54 -0400

    feat: update keyboard letter colors with each guess submitted

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,24 +1,26 @@
 import '../styles/Keyboard.css'
+import type { GuessResult } from '../utils/checkGuess'
 
 type KeyboardProps = {
     handleClick: (letter: string) => void
+    keyboardMap: Map<string, GuessResult['type']>
 }
 
-export const Keyboard = ({ handleClick }: KeyboardProps) => {
+export const Keyboard = ({ handleClick, keyboardMap }: KeyboardProps) => {
     const keyboardRows = new Map<number, string[]>([
         [1, ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']],
         [2, ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']],
         [3, ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL']]
     ])
-    
+
     return (
-        <div className="keyboard-container">
+        <div>
             {Array.from(keyboardRows.entries()).map(([rowNumber, letters]) => (
                 <div key={rowNumber} className="keyboard-row">
                     {letters.map((letter) => (
                         <button 
                             key={letter} 
-                            className="keyboard-key"
+                            className={`keyboard-key ${keyboardMap.get(letter)}-key`}
                             onClick={() => handleClick(letter)}>
                             {letter}
                         </button>
